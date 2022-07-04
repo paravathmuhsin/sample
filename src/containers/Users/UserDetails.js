3edfd7937471119fd4f57c352c24171d0e018269
Musthafa <musthafa.vp43@gmail.com>
2022-07-04 23:40:32 +0530

    new

--- src/containers/Users/UserDetails.js
+++ src/containers/Users/UserDetails.js
@@ -37,7 +37,7 @@ const UserDetails = () => {
                   endIcon={<SendIcon />}
                   style={{ fontSize: "12px", float : "right" }}
                 >
-                  Go Back
+                  View Users    
                 </Button>
             
             </Typography>
