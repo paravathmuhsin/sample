9ad5f133d1b21048f9e7ba438eca7584724d8335
yadualakodan <toyadu2610@gmail.com>
2022-07-06 07:29:19 +0100

    refreshing all

--- src/services/comments.service.js
+++
@@ -1,7 +0,0 @@
-import axiosInstance from "../utility/axios";
-
-export const getcomments = () =>
-  axiosInstance.get("/comments").then((res) => res.data);
-
-export const getComments = (id) =>
-  axiosInstance.get(`/comments/${id}`).then((res) => res.data);
